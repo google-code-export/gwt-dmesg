9cb9b5a27002411dac43f69ea368b286535f1041
kiss.gergely <kiss.gergely@19e8bf6a-d794-11de-b0e0-6bdc6d9bcfd0>
2009-11-22 20:43:40 +0000

    deleted failed release tag

--- tags/dmesg-1.0.0/src/test/java/org/gwt/dmesg/client/MyEnum.java
+++
@@ -1,8 +0,0 @@
-package org.gwt.dmesg.client;
-
-/**
- * Test enum for enum localization test.
- */
-public enum MyEnum {
-	FIRST, SECOND, THIRD
-}
--- tags/dmesg-1.0.0/src/test/java/org/gwt/dmesg/client/TestMessages.java
+++
@@ -1,9 +0,0 @@
-package org.gwt.dmesg.client;
-
-import java.util.Date;
-
-public interface TestMessages extends com.google.gwt.i18n.client.Messages {
-	public String myLabel();
-
-	public String parametric(String string, int i, Date date);
-}
